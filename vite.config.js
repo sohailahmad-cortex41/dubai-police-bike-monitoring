12a6fc2d4db84f003dece6789ecf2bbc773c930e
sohailahmad-dev <sohailahmaddev@gmail.com>
2025-10-15 13:47:44 +0500

    add Dockerfile,docker-compose.yml & configure vite for docker

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
     proxy: {
       "/api": {
         target: "http://localhost:5455/",
